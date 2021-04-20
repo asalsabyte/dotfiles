7b9b119c92434d2fc31ae317eca6ddb114fde35c
asalsabyte <asalsabyte@wearehackerone.com>
2021-04-19 19:07:35 -0500

    use zsh in macos

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 RCFILE="$HOME/.bashrc"
+
+if [ "$(uname)" == "Darwin" ]; then
+	RCFILE="$HOME/.zshrc"
+fi
+
+
+echo $RCFILE
+
 CONDITION='if [ -f "$HOME/.asalsabyte_dotfiles.sh" ]; then source "$HOME/.asalsabyte_dotfiles.sh"; fi'
 
 ## Copy files
