9afb84404e737095af603faeae12ca76f691fed7
Sergio Botero <sergiobuj@users.noreply.github.com>
2021-04-25 19:31:34 -0500

    install some go tooling

--- asalsabyte_dotfiles.sh
+++ asalsabyte_dotfiles.sh
@@ -2,3 +2,6 @@
 alias sudo='sudo '
 alias nmap_all='nmap -sC -sT -O -vv'
 
+export PATH="$PATH:$HOME/go/bin"
+
+complete -W "\$(gf -list)" gf
--- setup.sh
+++ setup.sh
@@ -6,6 +6,14 @@ if [ "$(uname)" == "Darwin" ]; then
 	RCFILE="$HOME/.zshrc"
 fi
 
+if command -v go &> /dev/null ; then
+	go get -u github.com/tomnomnom/assetfinder
+	go get -u github.com/tomnomnom/gf
+	go get -u github.com/tomnomnom/httprobe
+	go get -u github.com/tomnomnom/anew
+	go get -u github.com/tomnomnom/fff
+	go get -u github.com/tomnomnom/waybackurls
+fi
 
 echo $RCFILE
 
